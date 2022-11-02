ef0ed0418e56893a88464649cb094a19162a2c0f
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2022-11-02 12:12:06 +0100

    Renamed cli arg --flows to --structure

--- gen.py
+++ gen.py
@@ -10,7 +10,7 @@ from src import generate as _generate
 
 if __name__ == '__main__':
     """
-        Example: python gen.py --flows examples/pss_api_steam_v0.991.4_anonymized.json --enums examples/pss_v0.992_dump_enums.json --out bin
+        Example: python gen.py --structure examples/pss_api_steam_v0.991.4_anonymized.json --enums examples/pss_v0.992_dump_enums.json --out bin
     """
 
     # enable Windows support of colors
@@ -20,14 +20,14 @@ if __name__ == '__main__':
     ERR_ENUMS_NOT_FOUND = 2
 
     parser = _argparse.ArgumentParser()
-    parser.add_argument('--flows', type=str, required=True, help='Path to the flows JSON file to be used')
+    parser.add_argument('--structure', type=str, required=True, help='Path to the structure JSON file to be used')
     parser.add_argument('--enums', type=str, required=False, help='Path to the enumerations JSON file to be used')
     parser.add_argument('--out', type=str, required=True, help='Target directory for the generated files')
     parser.add_argument('--overwrite', action='store_true', default=False, help='Overwrite all files (by default only raw files will be overwritten)')
     args = parser.parse_args()
 
-    if not _os.path.isfile(args.flows):
-        print(f'{_Fore.RED}ERROR: Flows JSON file does not exist!{_Fore.RESET}')
+    if not _os.path.isfile(args.structure):
+        print(f'{_Fore.RED}ERROR: Structure JSON file does not exist!{_Fore.RESET}')
         _sys.exit(ERR_SERVICES_NOT_FOUND)
 
     if args.enums and not _os.path.isfile(args.enums):
@@ -36,7 +36,7 @@ if __name__ == '__main__':
 
     with _Timer() as t:
         output_directory = args.out.rstrip('/')
-        print(f'{_Fore.YELLOW} >>>{_Fore.RESET} Endpoints: {args.flows}')
+        print(f'{_Fore.YELLOW} >>>{_Fore.RESET} Endpoints: {args.structure}')
         if args.enums:
             print(f'{_Fore.YELLOW} >>>{_Fore.RESET} Enumerations: {args.enums}')
         print(f'{_Fore.YELLOW} >>>{_Fore.RESET} Output path: {output_directory}')
@@ -44,7 +44,7 @@ if __name__ == '__main__':
         print(f'{_Fore.BLUE} >>>{_Fore.RESET} Generating code...')
 
         _generate.generate_source_code(
-            args.flows,
+            args.structure,
             args.enums,
             output_directory,
             force_overwrite=args.overwrite,
