023972f66b483274ba79202157d1e6121a2b5546
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-03-04 18:00:02 +0100

    Added tests for src/generate.py

---
+++ tests/test_generate.py
@@ -0,0 +1,68 @@
+import src.generate as _generate
+
+
+def test___get_return_type__multiple_entity_collections():
+    response_structure = {
+        "AllianceService": {
+            "ListUsers": {
+              "Messages": {
+                "Message": {}
+              },
+              "Users": {
+                "User": {
+                  "Alliance": {},
+                  "UserSeason": {}
+                }
+              }
+            }
+        }
+    }
+    entity_names = ['Message', 'Ship', 'User']
+    expected_result = ('ListUsers', ['Messages', 'Users'])
+
+    assert _generate.__get_return_type(response_structure, entity_names) == expected_result
+
+
+def test___get_return_type__single_entity_type():
+    response_structure = {
+        "AllianceService": {
+            "ListAlliancesByRanking": {
+              "Alliances": {
+                "Alliance": {}
+              }
+            }
+        }
+    }
+    entity_names = ['Alliance', 'ItemDesign', 'User']
+    expected_result = ('Alliances', ['Alliance'])
+
+    assert _generate.__get_return_type(response_structure, entity_names) == expected_result
+
+
+def test___get_return_type__multiple_entity_types():
+    
+    response_structure = {
+        "ShipService": {
+            "InspectShip": {
+                "Ship": {
+                    "Characters": {},
+                    "Items": {},
+                    "Lifts": {},
+                    "Researches": {
+                        "Research": {}
+                    },
+                    "Rooms": {
+                        "Room": {}
+                    },
+                    "UserStarSystems": {
+                        "UserStarSystem": {}
+                    }
+                },
+                "User": {}
+            }
+        }
+    }
+    entity_names = ['Alliance', 'Character', 'Item', 'Lift', 'Research', 'Ship', 'ShipDesign', 'User', 'UserStarSystem']
+    expected_result = ('InspectShip', ['Ship', 'User'])
+
+    assert _generate.__get_return_type(response_structure, entity_names) == expected_result
