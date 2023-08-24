2487abe028fd464d16572bb72e508b3d7207c201
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-08-24 13:41:27 +0200

    Allow multiple input files for anonymize module

--- anon.py
+++ anon.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     ERR_INPUT_NOT_FOUND = 1
 
     parser = _argparse.ArgumentParser()
-    parser.add_argument('--in', dest='in_', type=str, required=True, help='Path to the flows file to be anonymized')
+    parser.add_argument('--in', dest='in_', type=str, nargs='+', required=True, help='Path to the flows file(s) to be anonymized')
     parser.add_argument('--out', type=str, required=True, help='Target directory for the anonymized flows file')
     args = parser.parse_args()
 
@@ -28,22 +28,23 @@ if __name__ == '__main__':
         _sys.exit(ERR_INPUT_NOT_FOUND)
 
     with _Timer() as t:
-        print(f'{_Fore.YELLOW} >>>{_Fore.RESET} Input file: {args.in_}')
+        print(f'{_Fore.YELLOW} >>>{_Fore.RESET} Input file(s): {args.in_}')
+        for input_file_path in args.in_:
+            print(f'{_Fore.YELLOW} >>>{_Fore.RESET}  - {input_file_path}')
         print(f'{_Fore.YELLOW} >>>{_Fore.RESET} Output path: {args.out}')
         print(f'{_Fore.BLUE} >>>{_Fore.RESET} Anonymizing captured flows...')
 
-        input_file_name_with_extension = _os.path.split(args.in_)[1]
-        output_file_name = f'{_os.path.splitext(input_file_name_with_extension)[0]}_anonymized.flows'
-        output_file_path = _os.path.join(args.out, output_file_name)
 
-        anonymized_flows = _anonymize.anynomize_flows(
-            args.in_
-        )
-        _anonymize.store_flows(
-            output_file_path,
-            anonymized_flows
-        )
-        print(f'{_Fore.BLUE} >>>{_Fore.RESET} Stored anonymized flows at: {output_file_path}')
+        for input_file_path in args.in_:
+            input_file_name_with_extension = _os.path.split(input_file_path)[1]
+            output_file_name = f'{_os.path.splitext(input_file_name_with_extension)[0]}_anonymized.flows'
+            output_file_path = _os.path.join(args.out, output_file_name)
+            anonymized_flows = _anonymize.anynomize_flows(input_file_path)
+            _anonymize.store_flows(
+                output_file_path,
+                anonymized_flows
+            )
+            print(f'{_Fore.BLUE} >>>{_Fore.RESET} Stored anonymized flows at: {output_file_path}')
 
         print(f'{_Fore.BLUE} >>>{_Fore.RESET} Done in {t.elapsed}s')
         _sys.exit(0)
